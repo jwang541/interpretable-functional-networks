91f921f12f8dc7741e3eba98232acc0b012b7ade
Jannik6928 <james.wang6928@gmail.com>
2023-06-09 21:24:45 -0400

    move visualize_attention functionality into test_attention

--- test_attention.py
+++ test_attention.py
@@ -1,19 +1,21 @@
 import argparse
-import sys
 import numpy as np
 import scipy
 import scipy.stats
 import torch
 import pandas as pd
+import random
+import matplotlib.pyplot as plt
 
 from datasets import NiiDataset
+from utils import *
 from models import Model
 
 
 
 # Evaluate the model on a .nii formatted simtb dataset
 
-# Usage: test_simtb.py -w WEIGHTS
+# Usage: python test_attention.py -w WEIGHTS -d DATASET [-s SUBJECT]
 
 if __name__ == '__main__':
 
@@ -35,29 +37,34 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     with torch.no_grad():
-        model = Model(k_maps=20)
+        model = Model(k_maps=20, debug=True)
         model.load_state_dict(torch.load(args.weights))
         model = model.to(device)
         model.eval()
 
-        # visualize fmri datasets
-        testset = NiiDataset(args.dataset, train=False, print_params=False, normalization='voxelwise')
+        # testset = NiiDataset(args.dataset, train=False, print_params=False, normalization='voxelwise')
+        testset = NiiDataset(args.dataset, train=False, print_params=True, normalization='voxelwise')
+        
         mri, mask = testset.__getitem__(args.subject)
-        mri = torch.unsqueeze(mri, dim=0).float().to(device)
-        mask = torch.unsqueeze(mask, dim=0).bool().to(device)
+        mri =  mri.float().to(device)
+        mask = mask.bool().to(device)
 
-        model_out = model(mri[0], mask[0])
+        model_in = mri * mask
+        model_in, rician_indices = add_rician_noise(model_in, mask, 0.25, std=1.0)
+        model_in, affine_indices = add_affine2d_noise(model_in, mask, 0.25, max_trans=0.05, max_angle=5.0)
 
-        mask = torch.reshape(mask, (-1,))
-        x = torch.reshape(mri[0], (mri[0].shape[0], -1))
+        model_out = model(model_in, mask)
+
+        mask_flat = torch.reshape(mask, (-1,))
+        x = torch.reshape(model_in, (model_in.shape[0], -1))
         y = torch.reshape(model_out, (model_out.shape[0], -1))
 
-        x = x[:, mask].cpu().numpy()
-        y = y[:, mask].cpu().numpy()
+        x = x[:, mask_flat].cpu().numpy()
+        y = y[:, mask_flat].cpu().numpy()
 
         ###############################################################################################################
 
-        # Test 1: Compute correlation between each time point and each FN
+        # Print correlation between each time point and each FN
         
         correlations = np.zeros((x.shape[0], y.shape[0]))
         for i in range(x.shape[0]):
@@ -70,14 +77,64 @@ if __name__ == '__main__':
         pd.set_option('display.max_rows', None)
         print('- Correlations -')
         print(frame)
+        print()
+
+        ###############################################################################################################
+
+        # Print r^2 values between each time point and FNs
 
         explained_variance = np.square(correlations)
-        explained_variance = explained_variance / explained_variance.max(axis=0)
 
         frame = pd.DataFrame(explained_variance).astype('float')
         pd.options.display.float_format = '{:,.5f}'.format
         pd.set_option('display.max_rows', None)
-        print('- Scaled r^2 -')
+        print('- Time point & FN r^2 -')
         print(frame)
+        print()
+
+        ###############################################################################################################
+
+        # Print which time points had noise added
+        print('- Noise indices -')
+        print('Rician:', rician_indices)
+        print('Affine:', affine_indices)
+        print()
+
+        ###############################################################################################################
+
+        # Print attention values
+
+        model_in = model_in.cpu()
+        model_out = model_out.cpu()
+
+        raw_attention = model.attention.last
+        col_max, _ = torch.max(raw_attention, dim=0)
+        normalized_attention = raw_attention / col_max
+
+        frame = pd.DataFrame(normalized_attention.cpu().numpy()).astype('float')
+        
+        pd.options.display.float_format = '{:,.3f}'.format
+        pd.set_option('display.max_rows', None)
+
+        print('- Attention -')
+        print(frame)
+        print()
+
+        ###############################################################################################################
+
+        # Display 20 randomly selected time points
+
+        tps = random.sample(range(model_in.shape[0]), min(30, model_in.shape[0]))
+        tps.sort()
+
+        fig, axes = plt.subplots(5, 6, figsize=(8, 8))
+        axes = axes.flatten()
+        for i in range(len(tps)):
+            axes[i].imshow(model_in[tps[i], 0], cmap='gray')
+            axes[i].axis('off')
+            axes[i].set_title('t={}'.format(tps[i]), fontsize=10, pad=2)
+        plt.tight_layout()
+        plt.show()
+
 
 
