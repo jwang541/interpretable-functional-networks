caaacdce0e614ca1cad50017a0c7b54e87802393
Jannik6928 <james.wang6928@gmail.com>
2023-06-09 21:23:10 -0400

    rician and 2d affine noise

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 import torch
+import torchvision.transforms as transforms
 
+import random
 
 
 # Differentiable function to compute the projection matrix of the columns of Y
@@ -36,3 +38,36 @@ def clustering_loss(x, y, eps=1e-8):
     y_recon = torch.einsum('tk, ks -> ts', tcs, x)
 
     return torch.sum(torch.square(y_recon - y))
+
+
+# Add rician noise with standard deviation std to each time point with probability p
+# and return indices which had noise added
+def add_rician_noise(x, mask, p, std):
+    with torch.no_grad():
+        noisy_x = x.clone()
+        noisy_indices = []
+
+        for t in range(x.shape[0]):
+            if random.random() < p:
+                noise1 = std * torch.randn_like(noisy_x[t])
+                noise2 = std * torch.randn_like(noisy_x[t])
+                noisy_x[t] = torch.sqrt(torch.square(noisy_x[t] + noise1) + torch.square(noise2))
+                noisy_indices.append(t)
+
+        return noisy_x * mask, noisy_indices
+
+
+# Apply a random affine transformation to each time point with probability p and
+# return indices which had noise added
+def add_affine2d_noise(x, mask, p, max_trans, max_angle):
+    with torch.no_grad():
+        noisy_x = x.clone()
+        noisy_indices = []
+
+        for t in range(x.shape[0]):
+            if random.random() < p:
+                affine = transforms.RandomAffine(degrees=max_angle, translate=(max_trans, max_trans))
+                noisy_x[t] = affine(noisy_x[t])
+                noisy_indices.append(t)
+
+        return noisy_x * mask, noisy_indices
